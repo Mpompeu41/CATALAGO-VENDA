bfe6af16ca29a7e6659ea9aaf72a5972e0ed3df0
Mpompeu41 <mfpp41@gmail.com>
2021-10-19 23:11:48 -0300

    commit

--- index.js
+++ index.js
@@ -1,106 +1,69 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require("path");
+let message = "";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
-let message = "";
-
-const Kimono = require("./models/kimono");
+const Kimonos = require("./models/kimono");
 
 app.get("/", async (req, res) => {
-  
-  const kimono = await Kimono.findAll(); 
-  
+  const kimono = await Kimonos.findAll();
+
+  setTimeout(() => {
+    message = "";
+  }, 1000);
   res.render("index", {
-    kimono, message
+    kimono,
+    message,
   });
 });
 
-app.get("/detalhes/:id", async (req, res) => { 
-  const kimonos = await Kimono.findByPk(req.params.id); 
-
+app.get("/detalhes/:id", async (req, res) => {
+  const kimono = await Kimonos.findByPk(req.params.id);
   res.render("detalhes", {
-    kimonos,
+    kimono,
   });
 });
 
 app.get("/cadastro", (req, res) => {
-  res.render("cadastro", {message});
-});
-
-app.post("/cadastro", async (req, res) => {
-
-  const { nome, tamanho, cor, preco, imagem } = req.body;
-
-  if (!nome) {
-    res.render("cadastro", {
-      message: "Nome é obrigatório",
-    });
-  }
-
-  else if (!tamanho) {
-    res.render("cadastro", {
-      message: "tamanho é obrigatório",
-    });
-  }
-  else if (!preco) {
-    res.render("cadastro", {
-      message: "preço é obrigatório",
-    });
-  }
-  else if (!imagem) {
-    res.render("cadastro", {
-      message: "Imagem é obrigatório",
-    });
-  }
-
-  else {
-    try {
-      const kimono = await Kimono.create({
-        nome,
-        tamanho,
-        cor,
-        preco,
-        imagem
-      });
-
-      res.redirect("/");
-    } catch (err) {
-      console.log(err);
-
-      res.render("cadastro", {
-        message: "Ocorreu um erro ao cadastrar do kimono!",
-      });
-    }
-  }
-});
-
-
-app.get("/editar/:id", async (req, res) => {
-  const kimono = await Kimono.findByPk(req.params.id);
-
-  if (!kimono) {
-    res.render("editar", {
-      kimono,
-      message: "kimono não encontrado!",
-    });
-  }
-
-  res.render("editar", {
-    kimono, message
+  res.render("cadastro", {
+    message,
   });
 });
 
+app.post("/new", async (req, res) => {
+  const { nome, tamanho, cor, preco, imagem } = req.body;
+
+  const kimono = await Kimonos.create({
+    nome,
+    tamanho,
+    cor,
+    preco,
+    imagem,
+  });
+  message = `O Kimonos ${kimono.nome} foi criado!!!`;
+  res.redirect("/");
+});
+
+app.get("/editar/:id", async (req, res) => {
+  const kimono = await Kimonos.findByPk(req.params.id);
+
+  res.render("editar", {
+    kimono,
+    message,
+  });
+});
 
 app.post("/editar/:id", async (req, res) => {
-  const kimono = await Kimono.findByPk(req.params.id);
+  const kimono = await Kimonos.findByPk(req.params.id);
 
-  const { nome, tamanho, cor, preco, imagem } = req.body;
+  const { nome, tamanho, cor, preco, imagem} = req.body;
 
   kimono.nome = nome;
   kimono.tamanho = tamanho;
@@ -109,49 +72,27 @@ app.post("/editar/:id", async (req, res) => {
   kimono.imagem = imagem;
 
   const kimonoEditado = await kimono.save();
-
-  res.render("editar", {
-    kimono: kimonoEditado,
-    message: "kimono editado com sucesso!",
-  });
+  kimonoEditado,
+    (message = `O kimono ${kimono.nome} foi Editado com sucesso!!`),
+    res.redirect("/");
 });
-
 
 app.get("/deletar/:id", async (req, res) => {
-  const kimono = await Kimono.findByPk(req.params.id);
-
-  if (!kimono) {
-    res.render("deletar", {
-      kimono,
-      message: "kimono não encontrado!",
-    });
-  }
+  const kimono = await Kimonos.findByPk(req.params.id);
 
   res.render("deletar", {
-    kimono, message
+    kimono,
+    message,
   });
 });
-
-
 app.post("/deletar/:id", async (req, res) => {
-  const kimono = await Kimono.findByPk(req.params.id);
-
-  if (!kimono) {
-    res.render("deletar", {
-      mensagem: "kimono não encontrado!",
-    });
-  }
+  const kimono = await Kimonos.findByPk(req.params.id);
 
   await kimono.destroy();
-
+  message = `Kimono ${kimono.nome} deletado com sucesso!`;
   res.redirect("/");
 });
 
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`))
-
-
-
-
-
-
-
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
